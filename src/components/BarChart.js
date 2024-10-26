8bde654c7c798653a80b0c52848330a96e9655d1
betawars@oregonstate.edu <betawars@oregonstate.edu>
2024-10-25 19:33:47 -0700

    cahnages_1

--- src/components/BarChart.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from "react";
-
-function BarChart({ chartData }) {
-  return <Fragment>
-    <h1>Trying to display something</h1>
-  </Fragment>;
-}
-
-export default BarChart;
